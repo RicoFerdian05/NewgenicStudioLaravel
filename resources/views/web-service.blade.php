6309db2a3db8dc372c3425a266271184ae66d784
Rico Ferdian <ferdianrico6@gmail.com>
2022-05-17 12:45:27 +0700

    first commit with laravel

---
+++ resources/views/web-service.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Service/Web Design</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/web-service.css">
+
+   <!-- Family Font Inter -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="navbar">
+        <div class="container">
+                <div class="col-md-6" >
+                    <img src="../icons/logo_newgenic.png" alt="">&nbsp;
+                    <span class="title align-middle"><span class="sub-title">new</span>genic studio</span>
+                </div>
+                <div class="col-md-6">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/web-service" >Service</a></li>
+                        <li><a href="/team">Team</a></li>
+                        <li><a href="/ourwork">Our Work</a></li>
+                        <li><a href="/lets-talk"><button>Lets Talk</button></a></li>
+                        <li>En</li>
+                    </ul>
+                </div>  
+        </div>
+    </div>
+
+    <div class="web">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="left">
+                        <h2>Web Design</h2>
+                        <h6>Newgenic Studio is committed to helping customers reach their business dreams. Providing perfect strategy, innovative creations and effective digital marketing solutions following newest standards and trends. Let's grow your brand to the next level!</h6>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="../images/Frame 55.png"> 
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <center>
+        <div class="service">
+            <div class="container">
+                <h2> Our Services </h2>
+                <h5>WEB SOLUTION</h5>
+                <div class="row">
+                    <div class="col-md-4" style="margin-left:0px;">
+                        <img src="../images/Group 88.png"><br>
+                        <h5>WordPress Website Design & Development</h5>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="../images/Group 89.png"><br>
+                        <h5>User Interface Design</h5>
+                    </div>
+                    <div class="col-md-4" style="margin-right:0px;">
+                        <img src="../images/Group 90.png"><br>
+                        <h5>Web Design</h5>
+                    </div>
+                </div>             
+            </div>
+        </div>
+
+        <div class="reason">
+            <div class="container">
+                <h2>Why Choose Us?  </h2>
+                <h5>WE ARE THE BEST</h5>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="content1">
+                            <h6>Modern Design</h6>
+                            <p>Providing perfect strategy and effective digital marketing solutions following newest standards and trends.</p> 
+                        </div>
+                        <div class="content2">
+                            <h6>User Experience</h6>
+                            <p>Providing perfect strategy and effective digital marketing solutions following newest standards and trends.</p> 
+                        </div>
+                        <div class="content3" style="margin-bottom:0px">
+                            <h6>Life Time Support</h6>
+                            <p>Providing perfect strategy and effective digital marketing solutions following newest standards and trends.</p> 
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-10">
+                                <img src="../images/image 9.png">
+                            </div>
+                            <div class="col-md-2">
+                                <img src="../images/Group 95.png" class="spc">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </center>
+
+    <div class="footer1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="../images/Illustration 2.png">
+                        </div>
+                        <div class="col-md-8">
+                            <h4>Join 569 more people in the waitlist</h4>
+                            <button>Create a project</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6" style="margin-left:160px;">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="../images/Illustration 4.png">
+                        </div>
+                        <div class="col-md-8">
+                            <h4>You want work with us?</h4>
+                            <button class="spc-button">Join our team</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+
+
+</body>
+</html>
